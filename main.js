0fd7c6f9185f96a70d45fa9bcf362d4f9ac61d15
jeremybarbosa <jbarbosa1430@gmail.com>
2021-05-01 18:50:59 -0500

    fixed some minor issues

--- main.js
+++ main.js
@@ -4,14 +4,10 @@ const pSub = document.querySelector(".uPSub");
 const themeSong = new Audio('sounds/Pokemon-Theme-Song.mp3');
 const pika = new Audio('sounds/19.mp3');
 
-
-
 pSub.addEventListener('click', function(){
     // console.log('i was clicked')
     const URL = ` https://pokeapi.co/api/v2/pokemon/${playerPokemon.value}`
     
-    pika.play();
-
     fetch(URL)
     
     .then((res) => res.json())
@@ -26,31 +22,31 @@ pSub.addEventListener('click', function(){
         .then((res)=> res.json())
         
         .then((dmg)=>{
-            // console.log(dmg)
-            // console.log(dmg.damage_relations.double_damage_to[0].name)
-            // let leftHtml = `<div class="leftPokemon">
-            // <h1>Name : ${json.name} </h1>
-            // <img src="${json.sprites.front_default}" alt=""></img>
-            // <h2>Type : ${json.types[0].type.name}</h2>
-            // <h2>Weak Against : ${dmg.damage_relations.double_damage_from[0].name}</h2>
-            // <h2>Strong Against : ${dmg.damage_relations.double_damage_to[0].name} </h2>
+            console.log(dmg)
+            console.log(dmg.damage_relations.double_damage_to[0].name)
+            let leftHtml = `<div class="leftPokemon">
+            <h1>Name : ${json.name} </h1>
+            <img src="${json.sprites.front_default}" alt=""></img>
+            <h2>Type : ${json.types[0].type.name}</h2>
+            <h2>Weak Against : ${dmg.damage_relations.double_damage_from[0].name}</h2>
+            <h2>Strong Against : ${dmg.damage_relations.double_damage_to[0].name} </h2>
             
-            // </div>`
-            // displayLeft.innerHTML += leftHtml;
+            </div>`
+            displayLeft.innerHTML = leftHtml;
             
         })
     })
+    pika.play();
 })
 
 // ----- Main right side=============================
 const ePokemon = document.querySelector('.enemyPoke');
 const ePSub = document.querySelector('.ePSub');
 
-
 ePSub.addEventListener('click', function(){
     // console.log('i was clicked')
     const URL = ` https://pokeapi.co/api/v2/pokemon/${ePokemon.value}`
-
+    
     pika.play();
     
     fetch(URL)
@@ -96,6 +92,51 @@ let normalMove = [];
 let chargedMove = [];
 let reqCandy = 0;
 let evoInto = "";
+let movesHTML = "";
+
+function normalTypePokemonMovesArr(){
+    const pokeMovesURL = `https://pokemon-go1.p.rapidapi.com/current_pokemon_moves.json?rapidapi-key=${key.value}`;
+    const movesEncoded = encodeURI(pokeMovesURL)
+    
+    fetch(movesEncoded)
+    
+    .then((res) => res.json())
+    
+    .then((moves)=>{
+        // for(const move of moves){
+        for(const move of Object.values(moves)){
+            if(move.form === "Normal"){
+                normMoves.push(move)
+            }
+            else{
+                ranMoves.push(move)
+            }
+            // console.log(normMoves)
+        }
+        for(const move of normMoves){
+            if(pokem.value === move.pokemon_name){
+                normalMove = move.fast_moves;
+                chargedMove = move.charged_moves;
+            }
+            // console.log(move)
+        }
+        console.log('Normal M',normalMove,'Charge M',chargedMove)
+        movesHTML = `<div class="moves">
+        <h3>Pokemons Avalible Attacks</h3>
+        <ul>
+        <ol><span id="att">Fast Attacks</span></li>
+        <br>
+        <li id="ATT">${normalMove}</li>
+        <br>
+        <ol><span id="att">Charged Attacks</span></li>
+        <br>
+        <li id="ATT">${chargedMove}</li>
+        <br>
+        </ul>
+        </div>`
+        console.log('Normal Move',normalMove,'charged Move',chargedMove)
+    })
+}
 
 apiSub.addEventListener('click', function(){
     const evoURL =  `https://pokemon-go1.p.rapidapi.com/pokemon_evolutions.json?rapidapi-key=${key.value}`;
@@ -120,62 +161,16 @@ apiSub.addEventListener('click', function(){
         }
         normalTypePokemonMovesArr();
         candyToEvolve();
-        console.log('Normal M',normalMove,'Charge M',chargedMove)
-        let moveHTML = `<div class="moves">
-        <h3>Pokemons Avalible Attacks</h3>
-        <ul>
         
-        <ol><span id="att">Fast Attacks</span></li>
-        <br>
-        <li>${normalMove}</li>
-        <br>
-        <ol><span id="att">Charged Attacks</span></li>
-        <br>
-        <li>${chargedMove}</li>
-        <br>
-        </ul>
-        </div>
-        <div class="candys">
-        <h3>Required Candies To Evolve <span id="pokemonName"></span></h3>
+        console.log('Normal M',normalMove,'Charge M',chargedMove)
+        let candyHtml = 
+        `<div class="candys">
+        <h3> Candies Required To Evolve </h3>
         <h3 id="howMany">${reqCandy}</h3>
         </div>`
-        info.innerHTML += moveHTML;
-        console.log('Normal M',normalMove,'Charge M',chargedMove)
+        info.innerHTML = movesHTML + candyHtml;
     })
 })
-
-
-function normalTypePokemonMovesArr(){
-    const pokeMovesURL = `https://pokemon-go1.p.rapidapi.com/current_pokemon_moves.json?rapidapi-key=${key.value}`;
-    const movesEncoded = encodeURI(pokeMovesURL)
-    
-    
-    fetch(movesEncoded)
-    
-    .then((res) => res.json())
-    
-    .then((moves)=>{
-        // for(const move of moves){
-        for(const move of Object.values(moves)){
-            if(move.form === "Normal"){
-                normMoves.push(move)
-            }
-            else{
-                ranMoves.push(move)
-            }
-            // console.log(normMoves)
-        }
-        for(const move of normMoves){
-            if(pokem.value === move.pokemon_name){
-                normalMove = move.fast_moves;
-                chargedMove = move.charged_moves;
-            }
-            // console.log(move)
-        }
-        console.log('Normal Move',normalMove,'charged Move',chargedMove)
-    })
-}
-normalTypePokemonMovesArr();
 
 function candyToEvolve(){
     for(const norm of normArr){
@@ -188,4 +183,3 @@ function candyToEvolve(){
     }
     console.log(reqCandy)
 }
-candyToEvolve();
